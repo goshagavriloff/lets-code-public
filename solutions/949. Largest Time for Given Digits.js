1567e7292d4ddf04625ca3565fd6fa498da6b254
goshagavriloff <54405253+goshagavriloff@users.noreply.github.com>
2020-09-06 19:26:11 +0300

    84 ms

--- solutions/949. Largest Time for Given Digits.js
+++ solutions/949. Largest Time for Given Digits.js
@@ -1,3 +1,25 @@
+/**
+ * @param {number[]} A
+ * @return {string}
+ */
+var largestTimeFromDigits = function(A) {
+   
+    let hours=getByCond(A);   
+    let isValidHours=hours[0]!=null;
+    
+    return isValidHours?getTime(hours):"";
+};
+​
+let getTime=(time)=>{
+    time.sort((a,b)=>{
+        return parseInt(b.hour)-parseInt(a.hour);
+    });
+    
+    return time[0].hour+':'+time[0].minutes;
+    
+}
+​
+let getByCond=(s)=>{
     let arr=[];
     let numbers=[0,1,2,3];
     for (let i=0;i<s.length;i++){
@@ -13,25 +35,3 @@
             }
             
         }
-    }
-    
-    return arr;
-}
-​
-let getMinutes=(numbers,s,i,j)=>{
-    
-    let result= numbers
-                .filter((el)=>{
-                    return (el!=i)&&(el!=j);
-                })
-                .reduce((acc,val)=>{
-                    return acc.concat(s[val]);
-                },[])
-                .sort((a,b)=>{
-                    return b-a;
-                })
-                .join('')   
-                ;
-    return parseInt(result)>59?result.split('').reverse().join(''):result;
-    
-}
