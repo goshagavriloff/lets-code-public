a61debcc6b6394b335f5552043b528d90e4a046f
goshagavriloff <54405253+goshagavriloff@users.noreply.github.com>
2020-10-28 14:43:59 +0300

    76 ms

---
+++ solutions/100. Same Tree.js
@@ -0,0 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+   [p,q]=[JSON.stringify(p),JSON.stringify(q)];
+    return p==q;
+};
