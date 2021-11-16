939048f0fcfb60ddc8eb1ff2ac46d178dcb67ebf
goshagavriloff <54405253+goshagavriloff@users.noreply.github.com>
2021-11-16 19:42:13 +0300

    392 ms

--- solutions/1609. Even Odd Tree.js
+++ solutions/1609. Even Odd Tree.js
@@ -38,3 +38,34 @@ var isEvenOddTree = function(root) {
     let odd=arr.filter((el,i)=>{return i%2!=0});
     
     let [isValidEven,isValidOdd]=[[even,0],[odd,1]].map((mass)=>{
+        return mass[0].every((el)=>{
+            let check=true;
+            let i=el.length-1;
+            
+            while(i>0){
+                
+                check&=calcIsOddEven[mass[1]](el[i]);
+                check&=calcIsIncr[mass[1]](el[i],el[i-1]);
+                
+                i--;
+            }
+            check&=calcIsOddEven[mass[1]](el[0]);
+            
+            return check;
+        });
+    });
+    let isValidNode=arr[0][0]%2!=0;
+    
+    
+    let isValid=isValidEven&&isValidOdd&&isValidNode;
+    return isValid;
+    
+};
+let calcIsOddEven={
+    0:(a)=>{return a%2!=0},
+    1:(a)=>{return a%2==0}
+};
+let calcIsIncr={
+    0:(a,b)=>{return a>b},
+    1:(a,b)=>{return a<b}
+};
